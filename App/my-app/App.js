9b28fdbe8dff4bc892e6e8a46a8c4074b9e4c123
jioh0000 <jopark24@kis.ac>
2023-02-11 10:55:57 +0900

    Update App.js

--- App/my-app/App.js
+++ App/my-app/App.js
@@ -1,13 +1,46 @@
 // Top part of the app
 // Where we import all the libraries that we want to use in this page.
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 
 // The function that is written here is that the actual part that is shown to the user
 export default function App() {
   // The backend part
+  const [x, setX] = useState("hello") // these variables are called Hook, or State
+  const [num, setNum] = useState(0)
 
+  // Creating a function. Method 1
+  const newFunction = () => {
+    setX("bye")
+    console.log(x)
+  }
 
+  // Creatomg a function. Method 2
+  function newFunction2(param1){
+    setNum(20)
+    console.log(param1)
+  }
+
+  function plus(){
+    setNum(num+1)
+  }
+  function minus(){
+    setNum(num-1)
+  }
+
+  // How to create and show an array
+  const [newArray, setNewArray] = useState([1,2,3,4,5,6,7,8,9])
+  const [strArr, setStrArr] = useState(["Hello", "Bye", "Hello World", "Computer", "Science", "Programming", "Coding"])
+  const [filtered, setFiltered] = useState([])
+  const [search, setSearch] = useState("")
+
+  function updateArray(e){
+    var search = e.toLowerCase()
+    var array = strArr
+    array = array.filter((e) => {return e.toLowerCase().includes(search)})
+    setFiltered(array)
+  }
 
   // The return part of the code is the front end part
   // All Tags such as Text, View have to be imported
@@ -16,35 +49,46 @@ export default function App() {
   // Button tag: making button in the screen
   // Anything that exists between the < > are called props, and they help definne what the tag should look like or do.
   // justifyContent: 세로 정열 방법, alignItems: 가로 정열 방법, flexDirection: 돌릴 방향 ('row' = 옆으로 눞히기)
+
+  // 중간에 화면에 표시되는 variable들이 바뀌기 위해서는 화면이 render가 되고 위해 변수를 이런식으로 만들어준다: const [num, setNum] = useState(0)  or 
+  // const [newArray, setNewArray] = useState([1,2,3,4,5,6,7,8,9])
+  // Array를 돌릴때는 .map(el, ind) --> el: each element, ind: index
   return (
     <View style={{
-      backgroundColor: 'red',
       flex: 1,
-      justifyContent: 'space-around',
       alignItems: 'center',
-      flexDirection: 'row'
-    }}> 
-      <View style={{
-        backgroundColor: 'blue',
-        height: 50,
-        width: 50
-      }}>
-
-      </View>
-      <View style={{
-        backgroundColor: 'green',
-        height: 50,
-        width: 50
-      }}>
-
-      </View>
-      <View style={{
-        backgroundColor: 'purple',
-        height: 50,
-        width: 50
-      }}>
-
-      </View>
+      justifyContent: 'center'
+    }}>
+      <TextInput
+        style={{
+          backgroundColor: '#F0EEED',
+          width: '80%',
+          padding: 10,
+          borderRadius: 10
+        }}
+        placeholder = {'Search'}
+        onChangeText={(e) => {
+          updateArray(e)
+        }}
+      />
+      {
+        strArr.map((el, ind) =>(  
+          <View style={{
+            backgroundColor: '#6096B4',
+            width: '80%',
+            marginVertical: 10,
+            padding: 10,
+            borderRadius: 10
+          }}>
+            <Text style={{
+              color: 'white'
+            }}>
+              Element is: {el}
+            </Text>
+          </View>
+        ))
+      }
+      
     </View>
   );
 }
@@ -53,7 +97,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
