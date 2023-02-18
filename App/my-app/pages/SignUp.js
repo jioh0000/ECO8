2aa23aa064565918247a145b7921569eb3fe692d
jioh0000 <jopark24@kis.ac>
2023-02-18 11:57:08 +0900

    Update SignUp.js

--- App/my-app/pages/SignUp.js
+++ App/my-app/pages/SignUp.js
@@ -1,25 +1,13 @@
 import React from "react"
-import { View, Text, StyleSheet, TextInput, Button } from "react-native"
+import { View, Text, StyleSheet,  } from "react-native"
 
 export default function SignUp() {
+
     return (
         <View style={styles.container}>
-            <Text style={{
-                fontSize:20,
-                fontWeight: "bold",
-            }}>
-                Adding Two Numbers
+            <Text>
+                Sign up
             </Text>
-            <TextInput style={{
-                backgroundColor: "#EEEEEE",
-                width: "20%"
-            }}
-            />
-            <TextInput style={{
-                backgroundColor: "#EEEEEE"
-            }}
-            />
-
         </View>
     )
 }
