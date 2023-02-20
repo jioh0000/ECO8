e63253f99ad3dbe9ceb141daeebcc8feff78ca11
jioh0000 <jopark24@kis.ac>
2023-02-20 14:26:51 +0900

    Create BottomSheet.js

---
+++ App/my-app/pages/BottomSheet.js
@@ -0,0 +1,66 @@
+import React, { useRef } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import BottomSheet from "reanimated-bottom-sheet"
+
+export default function BottomSheetPage(){
+    
+    const sheetRef = useRef(null)
+
+    const sheetContent = () => {
+        return (
+            <View style={{
+                backgroundColor: 'white',
+                height: 500
+            }}>
+                <View style={{
+                    height: 7,
+                    width: 78,
+                    backgroundColor: 'rgba(0,0,0,0.69)',
+                    borderRadius: 50,
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    marginTop: 5
+                }}>
+                </View>
+                <Text>
+                    Bottom Sheet
+                </Text>
+            </View>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={{
+                backgroundColor: '#3E54AC',
+                padding: 20
+            }}
+            onPress = {() => {
+                sheetRef.current.snapTo(2)
+            }}
+            >
+                <Text style={{
+                    color: 'white',
+                    fontWeight: 'bold'
+                }}>
+                    Open Botton Sheet
+                </Text>
+            </TouchableOpacity>
+            <BottomSheet
+                ref = {sheetRef}
+                snapPoints = {[0, 300, 500]}
+                initialSnap = {2}
+                enabledGestureInteraction = {true}
+                renderContent = {sheetContent}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
