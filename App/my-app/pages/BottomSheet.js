bdf963a1cb148eccd9e5fffc3d05e92887134bb9
jioh0000 <jopark24@kis.ac>
2023-02-25 11:51:41 +0900

    Update BottomSheet.js

--- App/my-app/pages/BottomSheet.js
+++ App/my-app/pages/BottomSheet.js
@@ -1,9 +1,9 @@
 import React, { useRef } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import BottomSheet from "reanimated-bottom-sheet"
+import Header from "../components/Header"
 
 export default function BottomSheetPage(){
-    
     const sheetRef = useRef(null)
 
     const sheetContent = () => {
@@ -31,34 +31,41 @@ export default function BottomSheetPage(){
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={{
-                backgroundColor: '#3E54AC',
-                padding: 20
-            }}
-            onPress = {() => {
-                sheetRef.current.snapTo(2)
-            }}
-            >
-                <Text style={{
-                    color: 'white',
-                    fontWeight: 'bold'
-                }}>
-                    Open Botton Sheet
-                </Text>
-            </TouchableOpacity>
-            <BottomSheet
-                ref = {sheetRef}
-                snapPoints = {[0, 300, 500]}
-                initialSnap = {2}
-                enabledGestureInteraction = {true}
-                renderContent = {sheetContent}
-            />
+            <Header title="BOTTOMSHEET" sheetRef = {sheetRef}/>
+            <View style = {styles.innerContainer}>
+                <TouchableOpacity style={{
+                    backgroundColor: '#3E54AC',
+                    padding: 20
+                }}
+                onPress = {() => {
+                    sheetRef.current.snapTo(2)
+                }}
+                >
+                    <Text style={{
+                        color: 'white',
+                        fontWeight: 'bold'
+                    }}>
+                        Open Botton Sheet
+                    </Text>
+                </TouchableOpacity>
+                <BottomSheet
+                    ref = {sheetRef}
+                    snapPoints = {[0, 300, 500]}
+                    initialSnap = {0}
+                    enabledGestureInteraction = {true}
+                    renderContent = {sheetContent}
+                />
+            </View>
         </View>
     )
 }
 
+
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+    },
+    innerContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
