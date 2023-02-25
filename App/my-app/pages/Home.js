17b0093224295f0b79822bca146ad8fcd77fbde7
jioh0000 <jopark24@kis.ac>
2023-02-25 11:51:45 +0900

    Update Home.js

--- App/my-app/pages/Home.js
+++ App/my-app/pages/Home.js
@@ -1,26 +1,34 @@
 // 모든 React-native 페이지에서 공통으로 들어가는 시작 코드 : Skeleton Code
-import React from "react";
+import React, { useState} from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import Header from "../components/Header";
 
 export default function Home(props){
+    const [number, setNumber] = useState(0)
 
     return (
         <View style={styles.container}>
-            <Text>
-                Home page
-            </Text>
-            <Button
-                title="Go to Settings"
-                onPress={() => {
-                    props.navigation.navigate("Settings")
-                }}
-            />
+            <Header title="HOME" number = {number} setNumber = {setNumber}/>
+            <View style = {styles.innerContainer}>
+                <Text>
+                    {number}
+                </Text>
+                <Button
+                    title="Go to Settings"
+                    onPress={() => {
+                        props.navigation.navigate("Settings")
+                    }}
+                />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+    },
+    innerContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
