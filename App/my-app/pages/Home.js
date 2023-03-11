81facbb87d717c68c22a7c14ba1dd1a36fdf3c95
jioh0000 <jopark24@kis.ac>
2023-03-11 20:06:11 +0900

    Update Home.js

--- App/my-app/pages/Home.js
+++ App/my-app/pages/Home.js
@@ -1,6 +1,6 @@
 // 모든 React-native 페이지에서 공통으로 들어가는 시작 코드 : Skeleton Code
 import React, { useState} from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, ImageBackground } from "react-native";
 import Header from "../components/Header";
 
 export default function Home(props){
@@ -9,6 +9,13 @@ export default function Home(props){
     return (
         <View style={styles.container}>
             <Header title="HOME" number = {number} setNumber = {setNumber}/>
+            <ImageBackground
+                source={require('../assets/homebackground.jpeg')}
+                style = {{
+                    width: '100%',
+                    heigt: '100%'
+                }}
+            />
             <View style = {styles.innerContainer}>
                 <Text>
                     {number}
